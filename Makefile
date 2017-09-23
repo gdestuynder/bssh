7f94d01b2ec1549d44899e0388431fb5bd583ff4
Guillaume Destuynder (kang) <kang+mozilla@insecure.ws>
2017-09-22 17:52:00 -0700

    Add pip stuff, cleanup, ready-up things for testing

---
+++ Makefile
@@ -0,0 +1,31 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2014-2017 Mozilla Corporation
+# Author: gdestuynder@mozilla.com
+PROJECT := bcorp
+VERSION := 0.1
+
+all:
+	./setup.py build
+
+install:
+	./setup.py install
+
+rpm:
+	fpm -s python -t rpm ./setup.py
+
+deb:
+	fpm -s python -t deb ./setup.py
+
+pypi:
+	 python setup.py sdist check upload --sign
+
+twine:
+	twine upload -s dist/$(PROJECT)-$(VERSION).tar.gz
+
+clean:
+	rm -rf *pyc
+	rm -rf build
+	rm -rf __pycache__
+	rm -rf dist
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2014-2017 Mozilla Corporation
+# Author: gdestuynder@mozilla.com
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "bcorp",
+        py_modules = [],
+        version = "0.1",
+        author = "Guillaume Destuynder",
+        author_email = "gdestuynder@mozilla.com",
+        description = ("A BeyondCorp style CLI client for the Federated Access Proxy"),
+        license = "MPL",
+        keywords = "ssh sts bcorp beyond corp federated access proxy sso iam",
+        url = "https://github.com/mozilla-iam/bcorp",
+        long_description = read('README.md'),
+        install_requires = [],
+        classifiers = [
+            "Development Status :: 5 - Production/Stable",
+            "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        ],
+)
